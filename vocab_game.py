0fcd0ce45dc451c842ab1610f44bb3f82305813e
404-42-paritchamon <56969@yupparaj.ac.th>
2026-09-07 09:48:58 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -7,23 +7,31 @@ if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
+if "ans3_val" not in st.session_state:
+    st.session_state.ans3_val = ""
+if "ans4_val" not in st.session_state:
+    st.session_state.ans4_val = ""
 
 
 
 def reset_game():
-    st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
-    st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
-    st.session_state.start = time.time()  # เริ่มเวลาใหม่
-    st.session_state.is_ended = False  # ปิด Dialog
+    st.session_state.ans1_val = "" 
+    st.session_state.ans2_val = ""  
+    st.session_state.ans3_val = "" 
+    st.session_state.ans4_val = ""  
+    st.session_state.start = time.time() 
+    st.session_state.is_ended = False  
 
 
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2):
+def show_result_dialog(ans1, ans2, ans3, ans4,):
     st.balloons()
     score = 0
 
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
+    u_ans3 = ans3.strip().lower()
+    u_ans4 = ans4.strip().lower()
 
     if u_ans1 == "apple":
         st.success("✅ ข้อ 1: ถูกต้อง")
